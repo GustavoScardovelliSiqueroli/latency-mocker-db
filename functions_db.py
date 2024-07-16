0397e7fe43088003302aff0776bd45ef4f4f76a2
Sergio Oseko <gustavosecardovelli@gmail.com>
2024-07-16 11:33:46 -0300

    init: project to mock data

---
+++ functions_db.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
---
+++ main.py
@@ -0,0 +1,3 @@
+import functions_db as f
+
+connection = f.create_server_connection("192.168.0.60", "gustavo", "123456")
